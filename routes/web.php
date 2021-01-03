e9bbd6ad229f848a3a06268c9acf34921a7d9761
EVOSandru6 <evosduple@mail.ru>
2021-01-03 12:10:55 +0300

    prepare minio driver

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Storage;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('storage-all', function () {
+    return Storage::disk('minio')->files();
 });
